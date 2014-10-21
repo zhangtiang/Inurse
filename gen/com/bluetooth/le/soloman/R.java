9075eda048a25201d9dce9dcb195e9c4622836a8
soloman <61859250@qq.com>
2014-10-21 17:23:15 +0800

    2.0.10 更新UI背景和图标

--- gen/com/bluetooth/le/soloman/R.java
+++ gen/com/bluetooth/le/soloman/R.java
@@ -14,24 +14,17 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int caroli=0x7f020000;
-        public static final int content_bg=0x7f020001;
-        public static final int distance=0x7f020002;
-        public static final int footer_bg=0x7f020003;
-        public static final int history=0x7f020004;
-        public static final int home=0x7f020005;
-        public static final int home1=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int logo=0x7f020008;
-        public static final int setting=0x7f020009;
-        public static final int setting1=0x7f02000a;
-        public static final int setting_page=0x7f02000b;
-        public static final int sleep=0x7f02000c;
-        public static final int sporttime=0x7f02000d;
-        public static final int step=0x7f02000e;
-        public static final int target=0x7f02000f;
-        public static final int tip=0x7f020010;
-        public static final int top_bg=0x7f020011;
+        public static final int fraghome_botton=0x7f020000;
+        public static final int home=0x7f020001;
+        public static final int home1=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int logo=0x7f020004;
+        public static final int setting=0x7f020005;
+        public static final int setting1=0x7f020006;
+        public static final int top_bg=0x7f020007;
+        public static final int wenduji=0x7f020008;
+        public static final int wenduji_checked=0x7f020009;
+        public static final int wenduji_selector=0x7f02000a;
     }
     public static final class id {
         public static final int autodel0=0x7f070017;
