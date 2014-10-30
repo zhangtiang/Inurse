f612673b4eb6fbc140c969676ef48e41524061a5
soloman <61859250@qq.com>
2014-10-30 21:54:31 +0800

    2.0.25 禁用upload print share按钮，恢复测量底部标签颜色

--- src/com/bluetooth/le/soloman/CeliangActivity.java
+++ src/com/bluetooth/le/soloman/CeliangActivity.java
@@ -17,6 +17,7 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 public class CeliangActivity extends FragmentActivity {
@@ -33,7 +34,8 @@ public class CeliangActivity extends FragmentActivity {
 
 
 	public MediaRecorder recorder;	
-	public ViewPager viewPager;	
+//	public ViewPager viewPager;	
+	public FrameLayout frag_container;
 	public ImageView iv_fanhui, iv_celianghome, iv_graph, iv_data, iv_celianghelp, iv_celiangsetting;
 
 	
@@ -49,7 +51,7 @@ public class CeliangActivity extends FragmentActivity {
 		
 		findView();
 		setOnClickListener();
-		initViewPager();
+//		initViewPager();
 		
 		
 		android.support.v4.app.FragmentManager fm =  getSupportFragmentManager();
@@ -57,16 +59,26 @@ public class CeliangActivity extends FragmentActivity {
 		
 		fragmentThemometer = (FragmentThemometer) fm.findFragmentById(R.layout.fragment_themometer);
 		
-		ft.add(R.id.viewPager,new FragmentThemometer(), "home");
-		ft.add(R.id.viewPager,new FragmentThemometer(), "graph");
+		ft.add(R.id.frag_container,new FragmentThemometer(), "home");
+//		ft.add(R.id.viewPager,new FragmentThemometerGraph(), "graph");
 		
 		//ft.replace(R.id.viewPager, fragmentSleep);
 		ft.commit();	
 
-	
+		iv_celianghome.setBackgroundResource(R.drawable.home1);
+		iv_graph.setBackgroundResource(R.drawable.graph);
+		iv_data.setBackgroundResource(R.drawable.data);
+		iv_celianghelp.setBackgroundResource(R.drawable.help);
+		iv_celiangsetting.setBackgroundResource(R.drawable.setting);
 
 	}
 
+	public void replaceFrag(Fragment f){
+		android.support.v4.app.FragmentManager fm =  getSupportFragmentManager();
+		ft = fm.beginTransaction();
+		ft.replace(R.id.frag_container,f);
+		ft.commit();
+	}
 	
 	private void setOnClickListener() {
 		// TODO Auto-generated method stub
@@ -86,7 +98,13 @@ public class CeliangActivity extends FragmentActivity {
 			@Override
 			public void onClick(View v) {
 				Log.i("info", "iv_celianghome onClicked");
-				viewPager.setCurrentItem(0);
+//				viewPager.setCurrentItem(0);
+				replaceFrag(new FragmentThemometer() );
+				iv_celianghome.setBackgroundResource(R.drawable.home1);
+				iv_graph.setBackgroundResource(R.drawable.graph);
+				iv_data.setBackgroundResource(R.drawable.data);
+				iv_celianghelp.setBackgroundResource(R.drawable.help);
+				iv_celiangsetting.setBackgroundResource(R.drawable.setting);
 			}
 		});
 		
@@ -95,7 +113,13 @@ public class CeliangActivity extends FragmentActivity {
 			@Override
 			public void onClick(View v) {
 				Log.i("info", "iv_graph onClicked");
-				viewPager.setCurrentItem(1);
+//				viewPager.setCurrentItem(1);
+				replaceFrag(new FragmentThemometerGraph() );
+				iv_graph.setBackgroundResource(R.drawable.graph1);
+				iv_celianghome.setBackgroundResource(R.drawable.home);
+				iv_data.setBackgroundResource(R.drawable.data);
+				iv_celianghelp.setBackgroundResource(R.drawable.help);
+				iv_celiangsetting.setBackgroundResource(R.drawable.setting);
 			}
 		});
 		
@@ -104,7 +128,13 @@ public class CeliangActivity extends FragmentActivity {
 			@Override
 			public void onClick(View v) {
 				Log.i("info", "iv_data onClicked");
-				viewPager.setCurrentItem(2);
+//				viewPager.setCurrentItem(2);
+				replaceFrag(new FragmentThemometerData() );
+				iv_graph.setBackgroundResource(R.drawable.graph);
+				iv_celianghome.setBackgroundResource(R.drawable.home);
+				iv_data.setBackgroundResource(R.drawable.data1);
+				iv_celianghelp.setBackgroundResource(R.drawable.help);
+				iv_celiangsetting.setBackgroundResource(R.drawable.setting);
 			}
 		});
 		
@@ -113,7 +143,13 @@ public class CeliangActivity extends FragmentActivity {
 			@Override
 			public void onClick(View v) {
 				Log.i("info", "iv_celianghelp onClicked");
-				viewPager.setCurrentItem(3);
+//				viewPager.setCurrentItem(3);
+				replaceFrag(new FragmentThemometerHelp() );
+				iv_graph.setBackgroundResource(R.drawable.graph);
+				iv_celianghome.setBackgroundResource(R.drawable.home);
+				iv_data.setBackgroundResource(R.drawable.data);
+				iv_celianghelp.setBackgroundResource(R.drawable.help1);
+				iv_celiangsetting.setBackgroundResource(R.drawable.setting);
 			}
 		});
 		
@@ -122,7 +158,13 @@ public class CeliangActivity extends FragmentActivity {
 			@Override
 			public void onClick(View v) {
 				Log.i("info", "iv_celiangsetting onClicked");
-				viewPager.setCurrentItem(4);
+//				viewPager.setCurrentItem(4);
+				replaceFrag(new FragmentThemometerSetting() );
+				iv_graph.setBackgroundResource(R.drawable.graph);
+				iv_celianghome.setBackgroundResource(R.drawable.home);
+				iv_data.setBackgroundResource(R.drawable.data);
+				iv_celianghelp.setBackgroundResource(R.drawable.help);
+				iv_celiangsetting.setBackgroundResource(R.drawable.setting1);
 			}
 		});
 	}
@@ -179,7 +221,8 @@ public class CeliangActivity extends FragmentActivity {
 
 	
 	public void findView(){
-		viewPager = (ViewPager) findViewById(R.id.viewPager);		
+//		viewPager = (ViewPager) findViewById(R.id.viewPager);
+		frag_container = (FrameLayout) findViewById(R.id.frag_container);
 	
 		iv_fanhui = (ImageView) findViewById(R.id.iv_fanhui);				
 		iv_celianghome = (ImageView) findViewById(R.id.iv_celianghome);
@@ -190,93 +233,89 @@ public class CeliangActivity extends FragmentActivity {
 	}
 
 	
-	private ArrayList<Fragment> fragmentArryList;  
-	public void initViewPager(){
-		//viewPager = (ViewPager)findViewById(R.id.viewpager);  
-		fragmentArryList = new ArrayList<Fragment>();  
- 
-		fragmentThemometer = new FragmentThemometer();
-		fragmentThemometerGraph = new FragmentThemometerGraph();
-		fragmentThemometerData = new FragmentThemometerData();
-		fragmentThemometerHelp = new FragmentThemometerHelp();
-//		fragmentThemometerSetting = new FragmentThemometerSetting();
-		fragmentThemometerSetting = new FragmentSetting();
-        
-		fragmentArryList.add(fragmentThemometer);  
-		fragmentArryList.add(fragmentThemometerGraph);
-		fragmentArryList.add(fragmentThemometerData);
-		fragmentArryList.add(fragmentThemometerHelp);
-		fragmentArryList.add(fragmentThemometerSetting);
-          
-        //给ViewPager设置适配器  
-		viewPager.setAdapter(new MyFragmentPagerAdapter(getSupportFragmentManager(), fragmentArryList));  
-		viewPager.setCurrentItem(0);//设置当前显示标签页为第一页  
-		viewPager.setOnPageChangeListener(new MyOnPageChangeListener());//页面变化时的监听器  		
-	}
-	
-	
-	public class MyOnPageChangeListener implements OnPageChangeListener {
-
-		@Override
-		public void onPageScrolled(int arg0, float arg1, int arg2) {
-			// TODO Auto-generated method stub
-			switch (arg0) {
-			case 0:
-				iv_celianghome.setBackgroundResource(R.drawable.home1);
-				iv_graph.setBackgroundResource(R.drawable.graph);
-				iv_data.setBackgroundResource(R.drawable.data);
-				iv_celianghelp.setBackgroundResource(R.drawable.help);
-				iv_celiangsetting.setBackgroundResource(R.drawable.setting);
-				break;
-			case 1:
-				iv_graph.setBackgroundResource(R.drawable.graph1);
-				iv_celianghome.setBackgroundResource(R.drawable.home);
-				iv_data.setBackgroundResource(R.drawable.data);
-				iv_celianghelp.setBackgroundResource(R.drawable.help);
-				iv_celiangsetting.setBackgroundResource(R.drawable.setting);
-				break;
-			case 2:
-				iv_graph.setBackgroundResource(R.drawable.graph);
-				iv_celianghome.setBackgroundResource(R.drawable.home);
-				iv_data.setBackgroundResource(R.drawable.data1);
-				iv_celianghelp.setBackgroundResource(R.drawable.help);
-				iv_celiangsetting.setBackgroundResource(R.drawable.setting);
-				break;
-			case 3:
-				iv_graph.setBackgroundResource(R.drawable.graph);
-				iv_celianghome.setBackgroundResource(R.drawable.home);
-				iv_data.setBackgroundResource(R.drawable.data);
-				iv_celianghelp.setBackgroundResource(R.drawable.help1);
-				iv_celiangsetting.setBackgroundResource(R.drawable.setting);
-				break;
-			case 4:
-				iv_graph.setBackgroundResource(R.drawable.graph);
-				iv_celianghome.setBackgroundResource(R.drawable.home);
-				iv_data.setBackgroundResource(R.drawable.data);
-				iv_celianghelp.setBackgroundResource(R.drawable.help);
-				iv_celiangsetting.setBackgroundResource(R.drawable.setting1);
-				break;
-			}
-		}
-
-		@Override
-		public void onPageScrollStateChanged(int arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		public int currentPageIndex;
-		@Override
-		public void onPageSelected(int arg0) {
-			// TODO Auto-generated method stub
+//	private ArrayList<Fragment> fragmentArryList;  
+//	public void initViewPager(){
+//		//viewPager = (ViewPager)findViewById(R.id.viewpager);  
+//		fragmentArryList = new ArrayList<Fragment>();  
+// 
+//		fragmentThemometer = new FragmentThemometer();
+//		fragmentThemometerGraph = new FragmentThemometerGraph();
+//		fragmentThemometerData = new FragmentThemometerData();
+//		fragmentThemometerHelp = new FragmentThemometerHelp();
+////		fragmentThemometerSetting = new FragmentThemometerSetting();
+//		fragmentThemometerSetting = new FragmentSetting();
+//        
+//		fragmentArryList.add(fragmentThemometer);  
+//		fragmentArryList.add(fragmentThemometerGraph);
+//		fragmentArryList.add(fragmentThemometerData);
+//		fragmentArryList.add(fragmentThemometerHelp);
+//		fragmentArryList.add(fragmentThemometerSetting);
+//          
+//        //给ViewPager设置适配器  
+////		viewPager.setAdapter(new MyFragmentPagerAdapter(getSupportFragmentManager(), fragmentArryList));  
+////		viewPager.setCurrentItem(0);//设置当前显示标签页为第一页  
+////		viewPager.setOnPageChangeListener(new MyOnPageChangeListener());//页面变化时的监听器  		
+//	}
+//	
+//	
+//	public class MyOnPageChangeListener implements OnPageChangeListener {
+//
+//		@Override
+//		public void onPageScrolled(int arg0, float arg1, int arg2) {
+//			// TODO Auto-generated method stub
 //			switch (arg0) {
 //			case 0:
-//				iv_home.setBackgroundResource(R.drawable.home1);
-//				iv_setting.setBackgroundResource(R.drawable.setting);
+//				iv_celianghome.setBackgroundResource(R.drawable.home1);
+//				iv_graph.setBackgroundResource(R.drawable.graph);
+//				iv_data.setBackgroundResource(R.drawable.data);
+//				iv_celianghelp.setBackgroundResource(R.drawable.help);
+//				iv_celiangsetting.setBackgroundResource(R.drawable.setting);
 //				break;
 //			case 1:
-//				iv_setting.setBackgroundResource(R.drawable.setting1);
-//				iv_home.setBackgroundResource(R.drawable.home);
+//				iv_graph.setBackgroundResource(R.drawable.graph1);
+//				iv_celianghome.setBackgroundResource(R.drawable.home);
+//				iv_data.setBackgroundResource(R.drawable.data);
+//				iv_celianghelp.setBackgroundResource(R.drawable.help);
+//				iv_celiangsetting.setBackgroundResource(R.drawable.setting);
+//				break;
+//			case 2:
+//				iv_graph.setBackgroundResource(R.drawable.graph);
+//				iv_celianghome.setBackgroundResource(R.drawable.home);
+//				iv_data.setBackgroundResource(R.drawable.data1);
+//				iv_celianghelp.setBackgroundResource(R.drawable.help);
+//				iv_celiangsetting.setBackgroundResource(R.drawable.setting);
+//				break;
+//			case 3:
+//				iv_graph.setBackgroundResource(R.drawable.graph);
+//				iv_celianghome.setBackgroundResource(R.drawable.home);
+//				iv_data.setBackgroundResource(R.drawable.data);
+//				iv_celianghelp.setBackgroundResource(R.drawable.help1);
+//				iv_celiangsetting.setBackgroundResource(R.drawable.setting);
+//				break;
+//			case 4:
+//				iv_graph.setBackgroundResource(R.drawable.graph);
+//				iv_celianghome.setBackgroundResource(R.drawable.home);
+//				iv_data.setBackgroundResource(R.drawable.data);
+//				iv_celianghelp.setBackgroundResource(R.drawable.help);
+//				iv_celiangsetting.setBackgroundResource(R.drawable.setting1);
+//				break;
+//			}
+//		}
+//
+//		@Override
+//		public void onPageScrollStateChanged(int arg0) {
+//			// TODO Auto-generated method stub
+//
+//		}
+//
+//		public int currentPageIndex;
+//		@Override
+//		public void onPageSelected(int arg0) {
+//			// TODO Auto-generated method stub
+//			switch (arg0) {
+//			case 0:
+//				break;
+//			case 1:
 //				break;
 //
 //			}
@@ -286,31 +325,31 @@ public class CeliangActivity extends FragmentActivity {
 //				fragmentArryList.get(arg0).onResume(); // 调用切换后Fargment的onResume()
 //			}
 //			currentPageIndex = arg0;
-		}
-		
-		
-		
-	}
-	
-	public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
-		ArrayList<Fragment> list;
-
-		public MyFragmentPagerAdapter(FragmentManager fm,
-				ArrayList<Fragment> list) {
-			super(fm);
-			this.list = list;
-
-		}
-
-		@Override
-		public int getCount() {
-			return list.size();
-		}
-
-		@Override
-		public Fragment getItem(int arg0) {
-			return list.get(arg0);
-		}			
-	} 
+//		}
+//		
+//		
+//		
+//	}
+//	
+//	public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+//		ArrayList<Fragment> list;
+//
+//		public MyFragmentPagerAdapter(FragmentManager fm,
+//				ArrayList<Fragment> list) {
+//			super(fm);
+//			this.list = list;
+//
+//		}
+//
+//		@Override
+//		public int getCount() {
+//			return list.size();
+//		}
+//
+//		@Override
+//		public Fragment getItem(int arg0) {
+//			return list.get(arg0);
+//		}			
+//	} 
 
 }
